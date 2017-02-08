e95455b4578547b97d0f4466c50230b8bd5d53d2
atbj505 <atbj505@hotmail.com>
2017-02-08 17:36:20 +0800

    project init

---
+++ pythonIpa.py
@@ -0,0 +1,22 @@
+# /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import datetime
+import email.mime.multipart
+import email.mime.text
+import ftplib
+import optparse
+import os
+import smtplib
+import sys
+
+
+def gitPull(parameter_list):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
